550167a083dc4c8f8c3cde964db517c0fd578390
up-left <left.right.forward@gmail.com>
2021-02-04 22:41:29 +0700

    Removed unnecessary downloads + added level lists

--- knyttlevels-parse/generate-download-script.py
+++ knyttlevels-parse/generate-download-script.py
@@ -1,5 +1,6 @@
 import csv
 import urllib.parse
+import os.path
 
 reader = csv.reader(open('../knyttlevels.csv'))
 
@@ -8,6 +9,7 @@ for row in reader:
     if row[0] not in ('downloaded', 'n/a'):
         link = row[6]
         fname = urllib.parse.unquote(link[link.rfind('/')+1:])
+        if os.path.isfile(f"../worlds/{fname}"): continue
         print(f'curl -o "../worlds/{fname}" http://knyttlevels.com/{link}')
         size += float(row[3][:-3]) * {'MB': 1024*1024, 'kB': 1024}[row[3][-2:]]
         
